42b5f429ea26111826efa5961afb2ad6d7b7945e
abderrahmane_Rabeh <130374509+abderrahmaneRabeh@users.noreply.github.com>
2024-08-22 23:15:03 +0100

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyB_IDUtl1kFvFsAxcwBNBrpGnR004hy4hg";
+const API_KEY = "test";
 
 const apiKey = API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
